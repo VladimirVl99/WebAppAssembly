95dee601629d3c3a1c7876fc82436bc692d610d9
Vladimir Vlasov <vova_vlasov_98@list.ru>
2022-11-22 21:36:06 +0700

    change#12.1

---
+++ TlgWebAppNet/TlgMethodNames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramWebAppNet
+{
+    public class TlgMethodNames
+    {
+        public string TelegramWebAppInit { get; }
+        public string CloseWebApp { get; }
+        public string SetViewOrderButton { get; }
+        public string SetMainButton { get; }
+        public string HideViewOrderButton { get; }
+        public string SetPayOrderButton { get; }
+    }
+}
